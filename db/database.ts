ae7061078b86f668a76b20bdefe93f12a2465415
Hidden Umah <anonymousumah96@gmail.com>
2026-07-26 22:37:30 +0000

    Added Database codes

---
+++ db/database.ts
@@ -0,0 +1,21 @@
+import * as SQLite from 'expo-sqlite';
+
+const db = SQLite.openDatabaseSync('students.db')
+
+export const initDB = () => {
+    db.execSync(`
+            CREATE TABLE IF NOT EXISTS students (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                student_id TEXT NOT NULL UNIQUE,
+                email TEXT NOT NULL UNIQUE .
+                picture TEXT,
+                description TEXT,
+                age INTEGER,
+                gender TEXT,
+                course TEXT NOT NULL
+            );
+        `)
+}
+
+export default db 
