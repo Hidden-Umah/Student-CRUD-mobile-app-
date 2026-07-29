d6afe7ecbe40ff96d566ed7a140818b9927fc271
Hidden Umah <anonymousumah96@gmail.com>
2026-07-29 23:00:56 +0000

    completed the index ( home screen ) - added some animations and also some advance responsive features

--- app/index.tsx
+++ app/index.tsx
@@ -1,35 +1,122 @@
-import { View, Text } from "react-native"
+import { View, Text, Pressable, useWindowDimensions , Image} from "react-native"
 import SideBar from "@/components/sidebar"
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { useEffect, useState, useRef } from "react";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import students from "../assets/images/students.png"
+import { Animated , Easing} from "react-native";
 
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import Feather from '@expo/vector-icons/Feather';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 export default function Home() {
+  const { width, height } = useWindowDimensions()
+  const [showSidebar, setShowSidebar] = useState(true);
+  const insets = useSafeAreaInsets()
+  const SIDEBAR_WIDTH = width * 0.22
+  const ICON_SIZE = SIDEBAR_WIDTH * 0.8
+
+  const sidebarItems = [
+    { id: 1, icon: <AntDesign name="user-add" size={26} color="#09090f" />, backgroundColor: "#d2f17bff" },
+    { id: 2, icon: <Ionicons name="people-outline" size={26} color="#09090f" />, backgroundColor: "#7bb8f1ff" },
+    { id: 3, icon: <Feather name="edit-2" size={26} color="white" />, backgroundColor: "#f8b02bff" },
+    { id: 4, icon: <MaterialCommunityIcons name="delete-outline" size={30} color="white" />, backgroundColor: "#eb4343ff" },
+  ]
+
+  const slideAnim = useRef(new Animated.Value(300)).current;
+
+  useEffect(()=>{
+    slideAnim.setValue(300)
+    Animated.timing(slideAnim , {
+      toValue: 0 ,
+      duration : 600 ,
+      easing: Easing.out(Easing.exp),
+      useNativeDriver: true
+    }).start()
+  } , [showSidebar])
+
+  function handleSidebar() {
+    setShowSidebar(!showSidebar)
+  }
+
   return (
-    <View
-      style={{
-        paddingTop: 50,
-        backgroundColor: "#09090f",
-        display: "flex",
-        flexDirection: "row",
-      }}
-    >
-      <SideBar />
-      <View>
-        <View
-          style={{
-            width: 270,
-            height: 700,
-            backgroundColor: "#212226",
-            borderTopLeftRadius: 40,
-            borderBottomLeftRadius: 10,
-            display: "flex",
-            flexDirection:"column",
+    <View style={{
+      paddingTop: 50,
+      backgroundColor: "#09090f",
+      flexDirection: "row",
+      flex: 1,
+      height: height
+    }}>
+      {showSidebar && <SideBar />}
+      <View style={{ flex: 1 }}>
+        <View style={{
+          height: showSidebar ? (height * 0.955) - insets.bottom - insets.top : height*1 ,
+          backgroundColor: "#212226",
+          borderTopLeftRadius: 40,
+          borderBottomLeftRadius: 10,
+          flexDirection: "column",
+          alignItems: "center",
+          paddingTop: 30,
+          borderTopRightRadius : showSidebar ? 0 : 40 
+        }}>
+
+          <View style={{
+            width: width * 0.6,
+            height: 40,
+            borderRadius: 20,
+            flexDirection: "row",
             alignItems: "center",
-            paddingTop: 30
-          }}
-        >
-            <View style={{ width: 200 , height : 40 , backgroundColor:"#09090f" , borderRadius: 20 ,display: "flex" , alignItems:"center"  , justifyContent:"center"}}>
-                <Text style={{ color:"white" , fontSize: 15 , fontWeight:800}}>Welcome</Text>
+            justifyContent: "center",
+            gap: 20,
+            marginBottom: 20
+          }}>
+
+            <View style={{
+              width: width * 0.4,
+              height: 40,
+              backgroundColor: "#09090f",
+              borderRadius: 20,
+              alignItems: "center",
+              justifyContent: "center"
+            }}> 
+              <Text style={{ color: "white", fontSize: 15, fontWeight: "800" }}>Welcome</Text>
             </View>
+
+
+            <Pressable onPress={handleSidebar}>
+              <View>
+                {showSidebar
+                  ? <MaterialIcons name="keyboard-arrow-left" size={24} color="white" />
+                  : <MaterialIcons name="keyboard-arrow-right" size={24} color="white" />
+                }
+              </View>
+            </Pressable>
+
+          </View>
+
+          <Image source={students}
+           style={{ width: showSidebar? width*0.7 : width*0.9  , height : 400 ,  borderRadius: 20 }}>
+
+          </Image>
+
+          <Animated.View style={{ transform: [{ translateY : slideAnim}] , display: "flex" , alignItems : "center" , padding: 20}} >
+            <Text style={{ marginTop : 20 , fontSize: 20 , fontWeight: 900 , color: 'white'}}>Student CRUD App</Text>
+            <Text style={{ marginTop : 10 , fontSize: 14, fontWeight: 400 , color: '#858585ff'}}>A mobile app to manage student records offline; add, view, edit and delete students with their photo, details and course info, all stored directly on your device.</Text>
+
+            {
+              !showSidebar && 
+              <View style={{ height: 70 , width: width*0.9 , marginTop: 20 , display: 'flex' , flexDirection: 'row' , alignItems: 'center' , justifyContent: 'space-evenly'}}>
+                {sidebarItems.map((item ,index) => (
+                  <View key={index} style={{ width: ICON_SIZE, height: ICON_SIZE, backgroundColor: item.backgroundColor, borderRadius: 22, alignItems: "center", justifyContent: "center" }}>
+                   {item.icon}
+                  </View>
+                ))}
+              </View>
+            }
+          </Animated.View>
+
 
         </View>
       </View>
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,41 +1,67 @@
-import { View, Text, Image } from "react-native"
+import { View, Text, Image, useWindowDimensions } from "react-native"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
+
 import AntDesign from '@expo/vector-icons/AntDesign';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import Feather from '@expo/vector-icons/Feather';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import studentImage from "../assets/images/student.jpeg"
 
 export default function SideBar() {
-  return (
-    <View
-      style={{
-        width: 90,
-        height: 900,
-        backgroundColor: "#09090f",
-        paddingTop: 30,
-        display:"flex",
-        alignItems: "center",
-        gap: 20,
-        position: "relative"
-      }}
-    >
-        <View style={{ width: 70 , height:70 , backgroundColor: "#d2f17bff" , borderRadius: 22, display: "flex" , alignItems:"center" ,justifyContent:"center"}}><AntDesign name="user-add" size={26} color="#09090f" /></View>
-        <View style={{ width: 70 , height:70 , backgroundColor: "#7bb8f1ff" , borderRadius: 22, display: "flex" , alignItems:"center" ,justifyContent:"center"}}><Ionicons name="people-outline" size={26} color="#09090f" /></View>
-        <View style={{ width: 70 , height:70 , backgroundColor: "#f8b02bff" , borderRadius: 22, display: "flex" , alignItems:"center" ,justifyContent:"center"}}><Feather name="edit-2" size={26} color="white" /></View>
-        <View style={{ width: 70 , height:70 , backgroundColor: "#eb4343ff" , borderRadius: 22, display: "flex" , alignItems:"center" ,justifyContent:"center"}}><MaterialCommunityIcons name="delete-outline" size={30} color="white" /></View>
 
-        <View style={{ position: "absolute" , height: 70 , width: 320 , backgroundColor: "#424242ff" , top : 665 , left:20 , zIndex:5 , borderRadius: 40 , display: "flex" , flexDirection: "row"}}>
-            <View style={{ display: "flex" , flexDirection: "row" , alignItems: "center" , justifyContent:"space-between"}}>
-                <Image
-                 source={studentImage}
-                 style={{width:80 , height: 80 ,borderRadius:40, marginRight: 5,  marginLeft: -5}}></Image>
-                <View style={{ width: "auto" ,height:70 , display:"flex" , alignItems:"flex-start", justifyContent:"center" , gap:6}}>
-                    <Text style={{ fontSize:18 , marginLeft: 10, fontWeight: 800 , color:"white"}}>Student CRUD APP</Text>
-                    <Text style={{marginLeft: 10, color:"#9b9b9bff"}}>Offline student records manager app</Text>
-                </View>
-            </View>
+  
+
+  const { width, height } = useWindowDimensions()
+  const SIDEBAR_WIDTH = width * 0.22
+  const ICON_SIZE = SIDEBAR_WIDTH * 0.8
+  const insets = useSafeAreaInsets()
+
+  return (
+    <View style={{
+      width: SIDEBAR_WIDTH,
+      height: height,
+      backgroundColor: "#09090f",
+      paddingTop: 30,
+      alignItems: "center",
+      gap: 20,
+      position: "relative"
+    }}>
+      <View style={{ width: ICON_SIZE, height: ICON_SIZE, backgroundColor: "#d2f17bff", borderRadius: 22, alignItems: "center", justifyContent: "center" }}>
+        <AntDesign name="user-add" size={26} color="#09090f" />
+      </View>
+      <View style={{ width: ICON_SIZE, height: ICON_SIZE, backgroundColor: "#7bb8f1ff", borderRadius: 22, alignItems: "center", justifyContent: "center" }}>
+        <Ionicons name="people-outline" size={26} color="#09090f" />
+      </View>
+      <View style={{ width: ICON_SIZE, height: ICON_SIZE, backgroundColor: "#f8b02bff", borderRadius: 22, alignItems: "center", justifyContent: "center" }}>
+        <Feather name="edit-2" size={26} color="white" />
+      </View>
+      <View style={{ width: ICON_SIZE, height: ICON_SIZE, backgroundColor: "#eb4343ff", borderRadius: 22, alignItems: "center", justifyContent: "center" }}>
+        <MaterialCommunityIcons name="delete-outline" size={30} color="white" />
+      </View>
+
+      
+      <View style={{
+        position: "absolute",
+        height: 70,
+        width: width * 0.85,
+        backgroundColor: "#424242ff",
+        bottom: insets.bottom +40 ,
+        left: SIDEBAR_WIDTH * 0.3,
+        zIndex: 5,
+        borderRadius: 40,
+        flexDirection: "row"
+      }}>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Image
+            source={studentImage}
+            style={{ width: 80, height: 80, borderRadius: 40, marginRight: 5, marginLeft: -5 }}
+          />
+          <View style={{ alignItems: "flex-start", justifyContent: "center", gap: 6 }}>
+            <Text style={{ fontSize: 16, marginLeft: 10, fontWeight: "800", color: "white" }}>Student CRUD APP</Text>
+            <Text style={{ marginLeft: 10, color: "#9b9b9bff", fontSize: 12 }}>Offline student records manager app</Text>
+          </View>
         </View>
+      </View>
     </View>
   )
 }
