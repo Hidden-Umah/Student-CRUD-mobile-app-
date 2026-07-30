d3c1f2579fdde61b0a4a01d660e5ad7de5dd2e44
Hidden Umah <anonymousumah96@gmail.com>
2026-07-30 09:03:57 +0000

    Added the initial codes for the various crud pages

--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,38 @@
-import { View, Text, Pressable, useWindowDimensions , Image} from "react-native"
+import { View, Text, TouchableOpacity, useWindowDimensions , Image} from "react-native"
 import SideBar from "@/components/sidebar"
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { useEffect, useState, useRef } from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import students from "../assets/images/students.png"
 import { Animated , Easing} from "react-native";
+import { RelativePathString, useRouter } from "expo-router";
 
 import AntDesign from '@expo/vector-icons/AntDesign';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import Feather from '@expo/vector-icons/Feather';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
+
+
+
+
+
 export default function Home() {
+
+
   const { width, height } = useWindowDimensions()
   const [showSidebar, setShowSidebar] = useState(true);
   const insets = useSafeAreaInsets()
   const SIDEBAR_WIDTH = width * 0.22
   const ICON_SIZE = SIDEBAR_WIDTH * 0.8
+  const router = useRouter()
+
 
   const sidebarItems = [
-    { id: 1, icon: <AntDesign name="user-add" size={26} color="#09090f" />, backgroundColor: "#d2f17bff" },
-    { id: 2, icon: <Ionicons name="people-outline" size={26} color="#09090f" />, backgroundColor: "#7bb8f1ff" },
-    { id: 3, icon: <Feather name="edit-2" size={26} color="white" />, backgroundColor: "#f8b02bff" },
-    { id: 4, icon: <MaterialCommunityIcons name="delete-outline" size={30} color="white" />, backgroundColor: "#eb4343ff" },
+    { id: 1, icon: <AntDesign name="user-add" size={26} color="#09090f" />, backgroundColor: "#d2f17bff" , route : "/create" as RelativePathString },
+    { id: 2, icon: <Ionicons name="people-outline" size={26} color="#09090f" />, backgroundColor: "#7bb8f1ff" , route :"/read" as RelativePathString },
+    { id: 3, icon: <Feather name="edit-2" size={26} color="white" />, backgroundColor: "#f8b02bff" , route :"/update" as RelativePathString },
+    { id: 4, icon: <MaterialCommunityIcons name="delete-outline" size={30} color="white" />, backgroundColor: "#eb4343ff" , route :"/delete" as RelativePathString},
   ]
 
   const slideAnim = useRef(new Animated.Value(300)).current;
@@ -85,14 +95,14 @@ export default function Home() {
             </View>
 
 
-            <Pressable onPress={handleSidebar}>
+            <TouchableOpacity onPress={handleSidebar}>
               <View>
                 {showSidebar
                   ? <MaterialIcons name="keyboard-arrow-left" size={24} color="white" />
                   : <MaterialIcons name="keyboard-arrow-right" size={24} color="white" />
                 }
               </View>
-            </Pressable>
+            </TouchableOpacity>
 
           </View>
 
@@ -109,9 +119,12 @@ export default function Home() {
               !showSidebar && 
               <View style={{ height: 70 , width: width*0.9 , marginTop: 20 , display: 'flex' , flexDirection: 'row' , alignItems: 'center' , justifyContent: 'space-evenly'}}>
                 {sidebarItems.map((item ,index) => (
-                  <View key={index} style={{ width: ICON_SIZE, height: ICON_SIZE, backgroundColor: item.backgroundColor, borderRadius: 22, alignItems: "center", justifyContent: "center" }}>
-                   {item.icon}
-                  </View>
+                  <TouchableOpacity
+                   key={index} 
+                   onPress={()=> router.push(item.route)}
+                   style={{ width: ICON_SIZE, height: ICON_SIZE, backgroundColor: item.backgroundColor, borderRadius: 22, alignItems: "center", justifyContent: "center" }}>
+                    {item.icon}
+                  </TouchableOpacity>
                 ))}
               </View>
             }
